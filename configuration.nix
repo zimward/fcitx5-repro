9ba8e3d92f4146d26518c4ea9e004ada3d3ee20f
zimward <96021122+zimward@users.noreply.github.com>
2024-08-31 10:45:01 +0200

    reproduction of issue

---
+++ configuration.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+{
+  users.users."repro" = {
+    initialPassword = "password";
+    isNormalUser = true;
+    shell = pkgs.nushell;
+  };
+  services.getty.autologinUser = "repro";
+  programs.sway = {
+    enable = true;
+    extraSessionCommands = ''
+      fcitx5 -d
+    '';
+  };
+  services.xserver.xkb.layout = "de";
+  services.xserver.xkb.variant = "dvorak";
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+  };
+  services.dbus.enable = true;
+  environment.sessionVariables = {
+    QT_IM_MODULE = "fcitx";
+    GTK_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
+  environment.systemPackages = [
+    pkgs.wmenu
+    pkgs.foot
+    pkgs.librewolf
+  ];
+  nixpkgs.hostPlatform = "x86_64-linux";
+  system.stateVersion = "24.11";
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      nixosConfigurations.repro = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+}
